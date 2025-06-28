c8fb86506e2e09f0c32eeb51a2b4d995b5e324bb
Mayra30345 <palmayra966@gmail.com>
2025-06-28 16:58:53 +0530

    Add files via upload

---
+++ Activity-2.py
@@ -0,0 +1,24 @@
+a=5
+print("Type of a: ", type(a))
+b= 3.2
+print("Type of b: ", type(b))
+c= "coding"
+print("Type of c: ", type(c))
+d= True
+print("Type of d: ", type(d))
+name="Penguin"
+age=21
+is_student= True
+weight= 37.8
+print(" Data types of name is: ", type(name))
+print(" Data types of age is: ", type(age))
+print(" Data types of is_student is: ", type(is_student))
+print(" Data types of weight is: ", type(weight))
+
+print("\n After type casting")
+age=str(age)
+print(age)
+print("Data type of age is: ", type(age))
+weight=int(weight)
+print(weight)
+print("Data type of weight is:  ",type(weight))
